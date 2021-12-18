77e8a91fee627553d7974045f04ad9bd95b2370e
keyli <keylinromero12@gmail.com>
2021-12-17 22:41:25 -0600

    Agregar archivos de proyecto.

---
+++ TareaMVVM/TareaMVVM/Models/Cargos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaMVVM.Models
+{
+    public class Cargos
+    {
+        public string Cargo { get; set; }
+        public static List<Cargos> GetCargos()
+        {
+            var cargo = new List<Cargos>()
+            {
+               
+              
+                new Cargos(){ Cargo ="IT" },
+                new Cargos(){ Cargo ="Supervisor" },
+                new Cargos(){ Cargo ="Acesor" },
+                 new Cargos(){ Cargo ="Administrador" }
+
+            };
+            return cargo;
+        }
+    }
+}
